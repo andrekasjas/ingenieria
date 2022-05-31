5728b66dae8abc2cb0a5fd20e989d719704d0161
Andres Mogollon <62920120+andrekasjas@users.noreply.github.com>
2022-05-31 16:30:51 -0500

    Update settings.py

--- ingenieria/settings.py
+++ ingenieria/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k+72n4w8_84enh)s^-()p&7bek+$5$4%^@&cg$hvl=zi2k1lyl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ingeniera.herokuapp.com']
 
 
 # Application definition

3f8b4c0020358b23b7bb451a2a329daf7ea5cc3d
Andres Mogollon <62920120+andrekasjas@users.noreply.github.com>
2022-06-01 19:33:09 -0500

    Update settings.py

--- ingenieria/settings.py
+++ ingenieria/settings.py
@@ -82,13 +82,13 @@ from decouple import config
 
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-    'default': dj_database_url.config(
-        default = config('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    #'default': dj_database_url.config(
+    #    default = config('DATABASE_URL')
+    #)
 }
 
 

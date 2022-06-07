5f7f6deaef17c022bf5cb97bebf39a1e83753e5d
andres mogollon <andresmogollob@gmail.com>
2022-06-07 17:41:23 -0500

    integrante

--- Archivo/forms.py
+++ Archivo/forms.py
@@ -1,20 +1,31 @@
 from django import forms
 from Integrante.models import integrante
+from Login.models import equipo
 
+OPTIONS =(
+            ("Sprint", "Sprint"),
+            ("Sprint planing", "Sprint planing"),
+            ("Sprint stand-up", "Sprint stand-up"),
+            ("Sprint review", "Sprint review"),
+            ("Sprint retrospective", "Sprint retrospective"),
+        )
+YES_NO = ((True,"Finalizado"),(False,"En curso"))
+
+def formulario_equipo(equipo_id):
+    class Formulario_archivos(forms.Form):
+        equipos = equipo.objects.filter(id = equipo_id)
+        integrante = equipos[0].integrante.all()
+        tarea = forms.CharField(max_length=60, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        tipo = forms.ChoiceField(choices = OPTIONS, widget = forms.Select(attrs={'class': 'form-control',}))
+        autor = forms.ModelChoiceField(queryset=integrante.order_by("-created"), widget = forms.Select(attrs={'class': 'form-control'}))
+        estado = forms.BooleanField(required=False, initial=False, widget=forms.RadioSelect(choices=YES_NO))
+        url_archivo = forms.FileField(label="Archivo", required=False)
+    return Formulario_archivos
 
 class Formulario_archivos(forms.Form):
-    OPTIONS =(
-        ("Sprint", "Sprint"),
-        ("Sprint planing", "Sprint planing"),
-        ("Sprint stand-up", "Sprint stand-up"),
-        ("Sprint review", "Sprint review"),
-        ("Sprint retrospective", "Sprint retrospective"),
-    )
-    YES_NO = ((True,"Finalizado"),(False,"En curso"))
-
-    tarea = forms.CharField(max_length=60, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    tipo = forms.ChoiceField(choices = OPTIONS, widget = forms.Select(attrs={'class': 'form-control',}))
-    autor = forms.ModelChoiceField(queryset=integrante.objects.all().order_by("-created"), widget = forms.Select(attrs={'class': 'form-control'}))
-    estado = forms.BooleanField(required=False, initial=False, widget=forms.RadioSelect(choices=YES_NO))
-    url_archivo = forms.FileField(label="Archivo", required=False)
+        tarea = forms.CharField(max_length=60, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+        tipo = forms.ChoiceField(choices = OPTIONS, widget = forms.Select(attrs={'class': 'form-control',}))
+        autor = forms.ModelChoiceField(queryset=integrante.objects.all().order_by("-created"), widget = forms.Select(attrs={'class': 'form-control'}))
+        estado = forms.BooleanField(required=False, initial=False, widget=forms.RadioSelect(choices=YES_NO))
+        url_archivo = forms.FileField(label="Archivo", required=False)
     
--- Archivo/views.py
+++ Archivo/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 
 from .models import archivo
 from Login.models import equipo
-from .forms import Formulario_archivos
+from .forms import formulario_equipo, Formulario_archivos
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
@@ -15,9 +15,9 @@ def archivos(request, equipo_id):
 
 
 def agregar_archivo(request, equipo_id):
-    hola = equipo.objects.filter(id = equipo_id)
-    for i in hola:
-        holas = i
+    equipos = equipo.objects.filter(id = equipo_id)
+    # for i in equipos:
+    #     equi = i
     if (request.method == "POST"):
         formulario = Formulario_archivos(request.POST, request.FILES)
         if (formulario.is_valid()):
@@ -28,7 +28,7 @@ def agregar_archivo(request, equipo_id):
             archivoc = request.FILES["url_archivo"]
             #handle_uploaded_file(archivoc)
             #url_archivo =  'static/archivos/'+archivoc.name
-            add = archivo.objects.create(nombre=archivoc, tarea=tarea, tipo=tipo, equipo=holas, autor=autor, estado=estado)
+            add = archivo.objects.create(nombre=archivoc, tarea=tarea, tipo=tipo, equipo=equipos[0], autor=autor, estado=estado)
             add.save()
 
             try:
@@ -37,7 +37,7 @@ def agregar_archivo(request, equipo_id):
             except:
                 return redirect('/archivo/agregar_archivo/'+str(equipo_id)+'/?novalido')
     else:
-        formulario = Formulario_archivos()
+        formulario = formulario_equipo(equipo_id)
 
     return render(request, "archivo/agregar_archivo.html",{'formulario':formulario})
 
